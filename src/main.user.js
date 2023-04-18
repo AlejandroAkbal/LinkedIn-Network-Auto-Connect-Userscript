d5208bd3f5a48a62c2e97751f95e992f015600d3
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-04-18 12:24:33 +0200

    chore: inline dependencies

--- src/main.user.js
+++ src/main.user.js
@@ -11,10 +11,73 @@
 // @updateURL       https://raw.githubusercontent.com/AlejandroAkbal/LinkedIn-Network-Auto-Connect-Userscript/main/src/main.user.js
 // @match           https://www.linkedin.com/mynetwork/
 // @grant           none
-// @require https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js#sha256-q2NOe7ej95rEv3NBnfvsseaWx/ADZmaOI+/eaeARj5Q=
 // @run-at          document-idle
 // ==/UserScript==
 
+// ========= Dependencies ========= //
+/**
+ * A utility function for userscripts that detects and handles AJAXed content.
+ *
+ * @see https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.2/waitForKeyElements.js
+ *
+ * Usage example:
+ *
+ *     function callback(domElement) {
+ *         domElement.innerHTML = "This text inserted by waitForKeyElements().";
+ *     }
+ *
+ *     waitForKeyElements("div.comments", callback);
+ *     // or
+ *     waitForKeyElements(selectorFunction, callback);
+ *
+ * @param {(string|function)} selectorOrFunction - The selector string or function.
+ * @param {function} callback - The callback function; takes a single DOM element as parameter.
+ *                              If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean} [waitOnce=true] - Whether to stop after the first elements are found.
+ * @param {number} [interval=300] - The time (ms) to wait between iterations.
+ * @param {number} [maxIntervals=-1] - The max number of intervals to run (negative number for unlimited).
+ */
+function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals) {
+  if (typeof waitOnce === "undefined") {
+    waitOnce = true;
+  }
+  if (typeof interval === "undefined") {
+    interval = 300;
+  }
+  if (typeof maxIntervals === "undefined") {
+    maxIntervals = -1;
+  }
+  var targetNodes = (typeof selectorOrFunction === "function")
+      ? selectorOrFunction()
+      : document.querySelectorAll(selectorOrFunction);
+
+  var targetsFound = targetNodes && targetNodes.length > 0;
+  if (targetsFound) {
+    targetNodes.forEach(function(targetNode) {
+      var attrAlreadyFound = "data-userscript-alreadyFound";
+      var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+      if (!alreadyFound) {
+        var cancelFound = callback(targetNode);
+        if (cancelFound) {
+          targetsFound = false;
+        }
+        else {
+          targetNode.setAttribute(attrAlreadyFound, true);
+        }
+      }
+    });
+  }
+
+  if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+    maxIntervals -= 1;
+    setTimeout(function() {
+      waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals);
+    }, interval);
+  }
+}
+
+
+// ========= Main Script ========= //
 ;(function () {
   'use strict'
 
